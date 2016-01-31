8a7730c075d825b7853676cc2b4d30f8924dbe9e
Raghav Sagar <raghavsagar92@gmail.com>
2016-02-01 04:00:59 +0530

    1. Fencing feature added. 2. Crash enabled. 3. Pending fix minor bugs

--- main.js
+++ main.js
@@ -45,15 +45,131 @@ var UtilityFunctions = {
   }
 }
 
+var Block = {
+  type: {
+    NONE: 0,
+    FENCE: 1
+  },
+  create: function(maxX, maxY, type) {
+    var blocks = [];
+    switch(type) {
+      case this.type.FENCE:
+        for(var i = 0; i <= maxX; ++i) {
+          blocks.push(new Position(i, 0));
+          blocks.push(new Position(i, maxY));
+        }
+        for(var i = 0; i <= maxY; ++i) {
+          blocks.push(new Position(0, i));
+          blocks.push(new Position(maxX, i));
+        }
+        break;
+      default:
+    }
+    return blocks;
+  }
+}
+
+var arena = {
+  //static
+  status: {
+    EMPTY: 0,
+    BLOCK: 1,
+    SNAKE: 2,
+    EGG: 3
+  },
+
+  //member
+  maxX: 0,
+  maxY: 0,
+  step: 10,
+  blockType: Block.type.FENCE,
+  grid: [[]],
+
+  init: function () {
+    // grid boundaries
+    this.maxX = Math.ceil(window.innerWidth/this.step) * this.step;
+    this.maxY = Math.ceil(window.innerHeight/this.step) * this.step;
+    var i,j,x,y;
+    // set grid empty 
+    this.grid = new Array(this.maxX+1);
+    for (i = 0; i <= this.maxX; ++i) {
+      this.grid[i] = new Array(this.maxY+1);
+      for (j = 0; j <= this.maxY; ++j) {
+        this.grid[i][j] = this.status.EMPTY;
+      }
+    }
+  },
+
+  initGrid: function() {
+    // set blocks
+    var blocks = Block.create(this.maxX, this.maxY, this.blockType);
+    for (i = 0; i < blocks.length; ++i) {
+      x = blocks[i].x;
+      y = blocks[i].y;
+      this.grid[x][y] = this.status.BLOCK;
+    }
+
+    // set initial snake
+    var head = snake.paths[0][0];
+    for (i = 0; i < snake.paths.length; ++i) {
+      for (j = 0; j < snake.paths[i].length - 1; ++j) {
+        var x1 = snake.paths[i][j].x;
+        var y1 = snake.paths[i][j].y;
+        var x2 = snake.paths[i][j+1].x;
+        var y2 = snake.paths[i][j+1].y;
+        if(x1 == x2) {
+          x = x1;
+          for(y = Math.min(y1, y2); y <= Math.max(y1, y2); ++y) {
+            if(!UtilityFunctions.isPositionEqual(head, new Position(x, y))) {
+              this.grid[x][y] = this.status.SNAKE;  
+            }
+          }
+        }
+        else if(y1 == y2) {
+          y = y1;
+          for(x = Math.min(x1, x2); x <= Math.max(x1, x2); ++x) {
+            if(!UtilityFunctions.isPositionEqual(head, new Position(x, y))) {
+              this.grid[x][y] = this.status.SNAKE;
+            }
+          }
+        }
+      }
+    }
+  },
+
+  isEmpty: function (position) {
+    return (this.grid[position.x][position.y] == this.status.EMPTY);
+  },
+  setEmpty: function (position) {
+    this.grid[position.x][position.y] = this.status.EMPTY;
+  },
+  isEgg: function (position) {
+    return (this.grid[position.x][position.y] == this.status.EGG);
+  },
+  isBlock: function (position) {
+    return (this.grid[position.x][position.y] == this.status.BLOCK);
+  },
+  isSnake: function (position) {
+    return (this.grid[position.x][position.y] == this.status.SNAKE);
+  },
+  setSnake: function (position) {
+    this.grid[position.x][position.y] = this.status.SNAKE;
+  }
+}
+
 var egg = {
   position: null,
-  step: 100,
-  reposition: function(maxX, maxY) {
-    this.position = new Position(Math.floor(Math.floor(Math.random() * maxX) / this.step) * this.step,
-                                 Math.floor(Math.floor(Math.random() * maxY) / this.step) * this.step);
+  init: function() {
+    this.reposition(arena.maxX, arena.maxY, arena.step);
+  },
+  reposition: function(maxX, maxY, step) {
+    this.position = new Position(Math.floor(Math.floor(Math.random() * maxX) / step) * step,
+                                 Math.floor(Math.floor(Math.random() * maxY) / step) * step);
   },
   draw: function() {
     $('canvas#arena').drawArc({
+      layer: true,
+      groups: ['egg'],
       strokeStyle: '#e60000',
       strokeWidth: 5,
       x: this.position.x, y: this.position.y,
@@ -66,21 +182,17 @@ var snake = {
   paths: [],
   direction: Direction.NONE,
   newDirection: Direction.NONE,
-  step: 100,
-  maxX: 0,
-  maxY: 0,
+  length: 50,
   init: function() {
-    this.maxX = Math.ceil(window.innerWidth/this.step) * this.step;
-    this.maxY = Math.ceil(window.innerHeight/this.step) * this.step;
     this.direction = Direction.RIGHT;
-    var head = new Position(this.maxX/2, this.maxY/2);
-    var tail = new Position(this.maxX/2 - 2*this.step, this.maxY/2);
+    var head = new Position(arena.maxX/2, arena.maxY/2);
+    var tail = new Position(arena.maxX/2 - this.length*arena.step, arena.maxY/2);
     var path = [head, tail];
     this.paths.push(path);
   },
 
   isInsideArena: function(position) {
-    return (position.x >= 0) && (position.x <= this.maxX) && (position.y >= 0) && (position.y <= this.maxY);
+    return (position.x >= 0) && (position.x <= arena.maxX) && (position.y >= 0) && (position.y <= arena.maxY);
   },
 
   hitBorder: function(position, direction) {
@@ -89,16 +201,16 @@ var snake = {
     var newHeadPath = [];
     switch(direction) {
       case Direction.UP:
-        head = new Position(position.x, this.maxY);
-        tail = new Position(position.x, this.maxY);
+        head = new Position(position.x, arena.maxY);
+        tail = new Position(position.x, arena.maxY);
         break;
       case Direction.DOWN:
         head = new Position(position.x, 0);
         tail = new Position(position.x, 0);
         break;
       case Direction.LEFT:
-        head = new Position(this.maxX, position.y);
-        tail = new Position(this.maxX, position.y);
+        head = new Position(arena.maxX, position.y);
+        tail = new Position(arena.maxX, position.y);
         break;
       case Direction.RIGHT:
         head = new Position(0, position.y);
@@ -135,49 +247,58 @@ var snake = {
     headPath = this.paths[0];
     var incLength = false;
     if(UtilityFunctions.isPositionEqual(headPath[0], egg.position)) {
-      egg.reposition(this.maxX, this.maxY);
+      egg.reposition(arena.maxX, arena.maxY, arena.step);
       incLength = true;
+    }
+
+    // Crash check
+    var head = this.paths[0][0];
+    if(arena.isSnake(head) || arena.isBlock(head)) {
+      console.log("Crashed at x: " + head.x + " y: " + head.y);
+      this.direction = Direction.NONE;
     }
 
     // Move head
     headPath = this.paths[0];
+    arena.setSnake(headPath[0]);
     this.movePosition(headPath[0], this.direction);
 
     // Move tail
     tailPath = this.paths[this.paths.length - 1];
     var trailingDirection = UtilityFunctions.getDirection(tailPath[tailPath.length-1], tailPath[tailPath.length-2]);
     if(!incLength) {
+      arena.setEmpty(tailPath[tailPath.length-1]);
       this.movePosition(tailPath[tailPath.length-1], trailingDirection);
     }
   },
-  
+
   movePosition: function(position, direction) {
     switch(direction) {
       case Direction.UP:
-        position.y -= this.step;
+        position.y -= arena.step;
         break;
       case Direction.DOWN:
-        position.y += this.step;
+        position.y += arena.step;
         break;
       case Direction.LEFT:
-        position.x -= this.step;
+        position.x -= arena.step;
         break;
       case Direction.RIGHT:
-        position.x += this.step;
+        position.x += arena.step;
         break;
       default:
     }
   },
-  updateDirection: function(direction) {
+  updateDirection: function() {
     if(this.newDirection == Direction.NONE)
       return;
-    if(direction == Direction.UP && this.direction == Direction.DOWN)
+    if(this.newDirection == Direction.UP && this.direction == Direction.DOWN)
       return;
-    if(direction == Direction.DOWN && this.direction == Direction.UP)
+    if(this.newDirection == Direction.DOWN && this.direction == Direction.UP)
       return;
-    if(direction == Direction.LEFT && this.direction == Direction.RIGHT)
+    if(this.newDirection == Direction.LEFT && this.direction == Direction.RIGHT)
       return;
-    if(direction == Direction.RIGHT && this.direction == Direction.LEFT)
+    if(this.newDirection == Direction.RIGHT && this.direction == Direction.LEFT)
       return;
     var headPath = this.paths[0];
     var newPosition = new Position(headPath[0].x, headPath[0].y);
@@ -189,6 +310,8 @@ var snake = {
     for (var i = 0; i < this.paths.length; ++i) {
       for (var j = 0; j < this.paths[i].length - 1; ++j) {
         $('canvas#arena').drawLine({
+          layer: true,
+          groups: ['snake'],
           strokeStyle: '#111',
           strokeWidth: 10,
           rounded: true,
@@ -202,15 +325,18 @@ var snake = {
 
 $(document).ready(function() {
   init();
+  arena.init();
+  egg.init();
   snake.init();
-  egg.reposition(snake.maxX, snake.maxY);
+  arena.initGrid();
   window.setInterval(function() {
     snake.updateDirection();
     snake.move();
-    $('canvas#arena').clearCanvas();
+    $('canvas#arena').removeLayers();
     egg.draw();
     snake.draw();
-  }, 250);  
+    $('canvas#arena').drawLayers();
+  }, 100);  
 });
 
 $(document).keydown(function(evt) {
